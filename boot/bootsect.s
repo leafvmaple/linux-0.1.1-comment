9d039366486e989aff0cca8b41911ebb681b7171
Zohar Lee <root@leafvmaple.com>
2024-04-02 22:09:46 +0800

    feat: update 20240402

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -29,6 +29,9 @@
 .set SYSSEG, 0x1000					# system loaded at 0x10000 (65536).
 .set ENDSEG, SYSSEG + SYSSIZE		# where to stop loading
 
+.set ROOT_DEV, 0x306
+
+.code16
 .globl begtext, begdata, begbss, endtext, enddata, endbss, start
 .text
 begtext:
@@ -40,35 +43,34 @@ begbss:
 
 # ROOT_DEV:	0x000 - same type of floppy as boot.
 #		0x301 - first partition on first drive etc
-.set ROOT_DEV, 0x306
 
 start:
 # ||| copy [0x7c00] 512B to [0x90000]
-	movw $BOOTSEG, ax
+	movw $BOOTSEG, %ax
 	movw %ax, %ds
-	movw $INITSEG, ax 
+	movw $INITSEG, %ax 
 	movw %ax, %es 
 	movw $0x100, %cx	# 256 Words = 512 Bytes
 	xorw %si, %si
 	xorw %di, %di
 	rep
 	movsw
-	ljmp $INITSEG, $go	# jump to INITSEG:go -> cs=0x9000, ip=go
+#	ljmp $INITSEG, $go	# jump to INITSEG:go -> cs=0x9000, ip=go
 
-go:	mov	%cs, %ax
-	mov	%ax, %ds
-	mov	%ax, %es
+go:	movw %cs, %ax
+	movw %ax, %ds
+	movw %ax, %es
 # put stack at 0x0xFF00.
-	mov	%ax, %ss
+	movw %ax, %ss
 # Stack in ss:sp=0x90400
-	mov $0xFF00, %sp	# arbitrary value >> 512 
+	movw $0xFF00, %sp	# arbitrary value >> 512 
 
 # load the setup-sectors directly after the bootblock.
 # Note that 'es' is already set up.
 
-# ||| Load Cylinder[0] Head[0] Sector[2] 4 Sectors(2048B) to [0x90200]
+# ||| Load Cylinder[0] Head[0] Sector[2] 4 Sectors(2048B) to ES:BX [0x90200]
 load_setup:
-	movw $0x0000, %dx	# drive 0, head 0
+	movw $0x0080, %dx	# drive 0, head 0
 	movw $0x0002, %cx	# sector 2, track 0
 	movw $0x0200, %bx	# address = 512, in INITSEG
 	movw $0x0200 + SETUPLEN, %ax	# service 2, nr of sectors
@@ -85,14 +87,13 @@ ok_load_setup:
 
 # Get disk drive parameters, specifically nr of sectors/track
 
-	movb $0x00, %dl
+	movb $0x80, %dl
 	movw $0x0800, %ax 		# AH=8 is get drive parameters
 	int	$0x13
 	movb $0x00, %ch 
-#	seg %cs
 	movw %cx, sectors		# [sectors] = sectors of driver
-	movw $INITSEG, %ax 
-	movw %ax, %es 
+	movw $INITSEG, %ax
+	movw %ax, %es
 
 # Print some inane message
 
@@ -102,7 +103,7 @@ ok_load_setup:
 	
 	movw $24, %cx
 	mov $0x0007, %bx		# page 0, attribute 7 (normal)
-	mov	$msg1, %bp
+	mov	msg1, %bp
 	mov	$0x1301, %ax 		# write string, move cursor
 	int	$0x10
 
@@ -119,29 +120,26 @@ ok_load_setup:
 # Otherwise, either /dev/PS0 (2,28) or /dev/at0 (2,8), depending
 # on the number of sectors that the BIOS reports currently.
 
-	seg cs
-	mov	ax, root_dev
-	cmp	ax, #0
+	movw root_dev, %ax
+	cmp	$0, %ax
 	jne	root_defined
-	seg cs
-	mov	bx, sectors
-	mov	ax, #0x0208		# /dev/ps0 - 1.2Mb
-	cmp	bx, #15
+	mov sectors, %bx
+	mov $0x0208, %ax		# /dev/ps0 - 1.2Mb
+	cmp	$15, %bx
 	je	root_defined
-	mov	ax, #0x021c		# /dev/PS0 - 1.44Mb
-	cmp	bx, #18
+	mov $0x021c, %ax		# /dev/PS0 - 1.44Mb
+	cmp	$18, %bx
 	je	root_defined
 undef_root:
 	jmp undef_root
 root_defined:
-	seg cs
-	mov	root_dev, ax
+	mov %ax, root_dev
 
 # after that (everyting loaded), we jump to
 # the setup-routine loaded directly after
 # the bootblock:
 
-	ljmp $SETUPSEG, 0
+	ljmp $SETUPSEG, $0
 
 # This routine loads the system at address 0x10000, making sure
 # no 64kB boundaries are crossed. We try to load it as fast as
@@ -155,81 +153,79 @@ track:	.word 0			# current track
 
 # move to es
 read_it:
-	mov ax, es
-	test ax, #0x0fff
+	mov %es, %ax
+	test $0x0fff, %ax
 die: jne die			# es must be at 64kB boundary
-	xor bx, bx		# bx is starting address within segment
+	xor %bx, %bx		# bx is starting address within segment
 rp_read:
-	mov ax, es
-	cmp ax, #ENDSEG		# have we loaded all yet?
+	mov %es, %ax 
+	cmp $ENDSEG, %ax	# have we loaded all yet?
 	jb ok1_read
 	ret
 ok1_read:
-	seg cs
-	mov ax, sectors
-	sub ax, sread        # 
-	mov cx, ax           # cx = sectors - 5
-	shl cx, #9           # cx * 512
-	add cx, bx
+	mov sectors, %ax
+	sub sread, %ax
+	mov %ax, %cx		# cx = sectors - 5
+	shl $9, %cx			# cx * 512
+	add %bx, %cx
 	jnc ok2_read
 	je ok2_read
-	xor ax, ax
-	sub ax, bx
-	shr ax, #9
+	xor %ax, %ax
+	sub %bx, %ax 
+	shr $9, %ax
 ok2_read:
 	call read_track
-	mov cx,ax
-	add ax,sread
-	seg cs
-	cmp ax,sectors
+	mov %ax, %cx
+	add sread, %ax
+	cmp sectors, %ax
 	jne ok3_read
-	mov ax,#1
-	sub ax,head
+	mov $1, %ax
+	sub head, %ax
 	jne ok4_read
-	inc track
+	incw track
 ok4_read:
-	mov head,ax
-	xor ax,ax
+	mov %ax, head
+	xor %ax, %ax
 ok3_read:
-	mov sread,ax
-	shl cx,#9
-	add bx,cx
+	mov %ax, sread
+	shl $9, %cx
+	add %cx, %bx
 	jnc rp_read
-	mov ax,es
-	add ax,#0x1000
-	mov es,ax
-	xor bx,bx
+	mov %es, %ax
+	add $0x1000, %ax
+	mov %ax, %es
+	xor %bx, %bx
 	jmp rp_read
 
 # read [ax] sectors from C[track] H[head] S[sread]
 read_track:
-	push ax
-	push bx
-	push cx
-	push dx
-	mov dx, track   # DL=track
-	mov cx, sread   # CL = spead
-	inc cx          # CL = ++sread, 读取sread号扇区
-	mov ch, dl      # CH=track, 读取track号柱面
-	mov dx, head    # DL=head
-	mov dh, dl      # DH=head
-	mov dl, #0      # DL=0, Driver 0
-	and dx, #0x0100 # DH=head&1, 读取第head&1磁头
-	mov ah, #2      # AH=2, 读磁盘
-	int 0x13
+	push %ax
+	push %bx
+	push %cx
+	push %dx
+	mov track, %dx		# DL = track
+	mov sread, %cx		# CL = spead
+	inc %cx				# CL = ++sread, 读取sread号扇区
+	mov %dl, %ch		# CH=track, 读取track号柱面
+	mov head, %dx		# DL=head
+	mov %dl, %dh		# DH=head
+	mov $0, %dl			# DL=0, Driver 0
+	and $0x0100, %dx	# DH=head&1, 读取第head&1磁头
+	mov $2, %ah			# AH=2, 读磁盘
+	int $0x13
 	jc bad_rt
-	pop dx
-	pop cx
-	pop bx
-	pop ax
+	pop %dx
+	pop %cx
+	pop %bx
+	pop %ax
 	ret
-bad_rt:	mov ax,#0
-	mov dx,#0
-	int 0x13
-	pop dx
-	pop cx
-	pop bx
-	pop ax
+bad_rt:	mov $0, %ax
+	mov $0, %dx
+	int $0x13
+	pop %dx
+	pop %cx
+	pop %bx
+	pop %ax
 	jmp read_track
 
 /*
@@ -238,11 +234,11 @@ bad_rt:	mov ax,#0
  * don't have to worry about it later.
  */
 kill_motor:
-	push dx
-	mov dx,#0x3f2
-	mov al,#0
-	outb
-	pop dx
+	push %dx
+	mov $0x3f2, %dx
+	mov $0, %al
+	outb %al, %dx
+	pop %dx
 	ret
 
 sectors:
